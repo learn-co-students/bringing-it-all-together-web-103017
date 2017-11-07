fd0fe550fb33b60dda7eb66c8a85f7536a34ef3d
github username <github email address>
2017-11-07 17:32:19 -0500

    finished

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,8 @@
 require 'sqlite3'
 require_relative '../lib/dog'
 
+# DB = {}
 DB = {:conn => SQLite3::Database.new("db/dogs.db")}
+# DB[:conn] = SQLite3::Database.new('db/dogs.db')
+
+DB[:conn].results_as_hash = true
